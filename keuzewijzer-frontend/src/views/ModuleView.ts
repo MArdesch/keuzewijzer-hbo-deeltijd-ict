46179852a0ea1d71606a47269fb79f1098d13a77
MArdesch <m.g.j.ardesch@live.nl>
2023-05-16 19:25:48 +0200

    hi
    hi

--- keuzewijzer-frontend/src/views/ModuleView.ts
+++ keuzewijzer-frontend/src/views/ModuleView.ts
@@ -3,10 +3,23 @@ import modules from "../mock/mock_module";
 
 export class ModuleView implements View {
     public template = `
+    <script id="template" type="text/x-handlebars-template">
+    <h1> {{name}} </h1>
+
+    <ol> {{module.name}}</ol>
+    </script>
+
+    <script type="text/javascript">
+    var modules = document.getElemenyById("template").innerHTML;
+    var result = Handlebars.comppile(modules)
+
     {{#each modules}}
         <h1>Year to select: {{year}}</h1>       
-        <p>Module name: {{name}}</p>    
+        <ol>
+        <li>Module name: {{name}}</li>    
+        </ol>
     {{/each}}
+    </script>
     `;    
     public data = modules;
 }
